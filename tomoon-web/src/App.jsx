1c15a3bcc12d0f745b52acc04c041fde00d2ee73
Chenx Dust <chenx_dust@outlook.com>
2025-05-30 03:01:35 +0800

    feat: brand new front end

--- tomoon-web/src/App.jsx
+++ tomoon-web/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import Swal from 'sweetalert2'
 import axios from 'axios'
+import color from 'tailwindcss/colors'
 
 function App() {
   const [url, setUrl] = useState("");
@@ -17,27 +16,62 @@ function App() {
   };
 
   return (
-    <div style={{ background: '#FAFFE3', minHeight: '100vh' }} className='flex justify-center items-center'>
+    <div className='flex justify-center items-center min-h-screen bg-white dark:bg-gray-900'>
       <div className='container grid grid-cols-12'>
         <div className='col-span-12 lg:col-start-2 lg:col-span-10'>
           <div className='flex justify-center items-center min-h-screen flex-col gap-4'>
-            <h1 className="text-6xl tomoon-title">
-              Tomoon
+            <h1 className='text-6xl text-gray-800 dark:text-gray-200 text-center'>
+              To Moon
             </h1>
-            <div className="flex items-center gap-2">
-              <label className="switch">
-                <input type="checkbox" checked={isSubscribed} onChange={handleToggleChange} />
-                <span className="slider round"></span>
-              </label>
-              <span className="text-xl tomoon-title">订阅转换</span>
-            </div>
-            <div className='w-full flex px-2'>
-              <input id="input-url" className='tomoon-input grow lg:text-2xl text-xl pl-5' type="text" placeholder='Clash 订阅链接' value={url} onChange={handleUrlChange} />
-              <button className='tomoon-button text-4xl flex justify-center items-center' onClick={() => { on_download_btn_click(url, isSubscribed) }}>
-                <svg className="tomoon-svg" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <p className='px-2 text-gray-500 dark:text-gray-400 text-center'>
+              支持 Mihomo/Clash 订阅，其他形式请启用订阅转换
+            </p>
+            <div className='w-full flex px-2 h-12'>
+              <input id="input-url" className='grow
+              bg-gray-100
+              dark:bg-gray-200
+              text-xl
+              pl-5
+              rounded-l-full
+              text-gray-700
+              dark:text-gray-900' type="text" placeholder='订阅链接' value={url} onChange={handleUrlChange} />
+              <button className='w-12
+                bg-indigo-500
+                dark:bg-indigo-700
+                flex
+                justify-center
+                items-center
+                rounded-r-full' onClick={() => { onDownloadBtnClick(url, isSubscribed) }}>
+                <svg className="h-6 w-6" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M0.0228571 48L48 24L0.0228571 0L0 18.6667L34.2857 24L0 29.3333L0.0228571 48Z" fill="white" />
                 </svg>
               </button>
+            </div>
+            <div className="relative inline-flex items-center cursor-pointer gap-2">
+              <label className="switch">
+                <input type="checkbox" className="sr-only peer" checked={isSubscribed} onChange={handleToggleChange} />
+                <div className="w-10 h-6
+                  bg-gray-400
+                  dark:bg-gray-700
+                  peer-focus:outline-none
+                  rounded-full
+                  peer
+                  peer-checked:after:translate-x-4
+                  peer-checked:after:border-white
+                  after:content-['']
+                  after:absolute
+                  after:top-[6px]
+                  after:left-[4px]
+                  after:bg-white
+                  after:dark:bg-gray-300
+                  after:rounded-full
+                  after:h-4
+                  after:w-4
+                  after:transition-all
+                  peer-checked:bg-indigo-400
+                  peer-checked:dark:bg-indigo-800"/>
+              </label>
+              <span className="text-xl text-gray-500 dark:text-gray-300" onClick={handleToggleChange}>启用订阅转换</span>
             </div>
           </div>
         </div>
@@ -46,15 +80,33 @@ function App() {
   );
 }
 
-const on_download_btn_click = (url, isSubscribed) => {
+const getColorSet = () => {
+  // 根据 tailwind 是否夜间主题判断
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? {
+      icon: color.indigo[400],
+      background: color.gray[900],
+      text: color.gray[200],
+      button: color.indigo[500]
+    }
+    : {
+      icon: color.indigo[400],
+      background: color.gray[50],
+      text: color.gray[800],
+      button: color.indigo[500]
+    };
+};
+const onDownloadBtnClick = (url, isSubscribed) => {
+  const colorSet = getColorSet();
   let baseHost = '/';
   if (import.meta.env.DEV) {
     baseHost = 'http://127.0.0.1:55556/';
   }
   Swal.fire({
-    iconColor: '#5E5F55',
-    confirmButtonColor: '#5A6242',
-    background: '#DEE7BF',
+    iconColor: colorSet.icon,
+    confirmButtonColor: colorSet.button,
+    background: colorSet.background,
+    color: colorSet.text,
     title: "下载中",
     text: "正在下载订阅配置，请稍等......",
     icon: "info"
@@ -68,31 +120,50 @@ const on_download_btn_click = (url, isSubscribed) => {
   }).then((response) => {
     if (response.status === 200) {
       Swal.fire({
+        iconColor: colorSet.icon,
+        confirmButtonColor: colorSet.button,
+        background: colorSet.background,
+        color: colorSet.text,
         icon: 'success',
-        iconColor: '#5E5F55',
         title: '完成',
         text: '已添加订阅',
-        confirmButtonColor: '#5A6242',
-        background: '#DEE7BF'
       });
     } else {
       Swal.fire({
+        iconColor: colorSet.icon,
+        confirmButtonColor: colorSet.button,
+        background: colorSet.background,
+        color: colorSet.text,
         icon: 'error',
-        iconColor: '#5E5F55',
         title: '后端失败',
-        html: `错误状态: ${response.status}<br>错误信息: ${response.data?.error?.message}`,
-        confirmButtonColor: '#5A6242',
-        background: '#DEE7BF'
+        html: `<b>错误状态: </b>
+        <code>
+          ${response.status}
+        </code>
+        <br>
+        <b>错误信息: </b>
+        <code>
+          ${response.data?.error?.message}
+        </code>`,
       });
     }
   }).catch(error => {
     Swal.fire({
+      iconColor: colorSet.icon,
+      confirmButtonColor: colorSet.button,
+      background: colorSet.background,
+      color: colorSet.text,
       icon: 'error',
-      iconColor: '#5E5F55',
       title: '请求失败',
-      html: `错误类型: ${error.name}<br>错误信息: ${error?.message}`,
-      confirmButtonColor: '#5A6242',
-      background: '#DEE7BF'
+      html: `<b>错误类型: </b>
+      <code>
+        ${error.name}
+      </code>
+      <br>
+      <b>错误信息: </b>
+      <code>
+      ${error?.message}
+      </code>`,
     });
   });
 
